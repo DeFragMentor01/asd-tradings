8b09accf2bef14aa2b245a50cdd255bf99f3b922
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2025-01-21 12:18:41 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Maintenance from './components/Maintenance';
+import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <Maintenance />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Maintenance />} />
+          <Route 
+            path="/613613" 
+            element={
+              <>
+                <Navbar />
+                <Hero />
+                <About />
+                <Services />
+                <Portfolio />
+                <Contact />
+                <Footer />
+              </>
+            } 
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
